730ff7860554e296c484d18323cadbea8ad42c31
aldrinhernando <aldrinh@live.com>
2015-02-12 20:32:14 -0700

    mini-paint

---
+++ mini-paint/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('.box').on('click', function() {
+        $(this).addClass(color);
+    });
+
+    $('.box').on('dblclick', function() {
+        $(this).removeClass('white');
+    });
+
+    $('#reset').on('click', function() {
+        $('.box').removeClass(colors);
+    })
+
+    var color = 'white';
+    var colors = 'white red blue green yellow';
+    $('#red').on('click', function() {
+        color = 'red';
+    });
+    $('#green').on('click', function() {
+        color = 'green';
+    });
+    $('#blue').on('click', function() {
+        color = 'blue';
+    });
+    $('#yellow').on('click', function() {
+        color = 'yellow';
+    });
+    $('#white').on('click', function() {
+        color = 'white';
+    });
+});
+
